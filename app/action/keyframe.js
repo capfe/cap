ea1ddc8e7f691d028969359603bee7171b6e9b09
lixijun <lixijun@baidu.com>
2016-08-10 17:38:02 +0800

    fix bug: when the frameIndex equal to 0, the "keyframe/add" cannot get origin value

--- app/action/keyframe.js
+++ app/action/keyframe.js
@@ -9,6 +9,7 @@ const Router = require('koa-router');
 const router = new Router();
 const KeyframeModel = require('../model/Keyframe.js');
 const KeyframeLayerModel = require('../model/KeyframeLayer.js');
+const LayerModel = require('../model/Layer.js');
 
 router.prefix('/keyframes');
 
@@ -16,6 +17,7 @@ router.post('/add', function* () {
     const dbModels = yield this.getDBModels();
     const keyframeModel = KeyframeModel.getInstance(dbModels);
     const keyframeLayerModel = KeyframeLayerModel.getInstance(dbModels);
+    const layerModel = LayerModel.getInstance(dbModels);
 
     const params = this.request.body;
     const projectid = params.projectid;
@@ -28,19 +30,30 @@ router.post('/add', function* () {
     const fx = 'linear';
     const keyframe = yield keyframeModel.fetchKeyframe({ index, projectid });
     const keyframes = yield keyframeModel.fetchKeyframes(projectid);
+    const layers = yield layerModel.fetchLayers(projectid);
 
     let obj = {};
     let flag = false;
 
-    // 找到前一帧，并复制前一帧的属性值
-    for (let i = index - 1; i >= 0; i--) {
-        const item = keyframes[i];
-        if (item) {
-            for (let layer of item.layers) {
-                console.log(layer.layerid, layer[prop]);
-                if (layer.layerid === layerid && layer[prop]) {
-                    obj = layer[prop];
-                    flag = true;
+    // 如果为第一帧 应该取layer的默认属性
+    if (+index === 0) {
+        for (let layer of layers) {
+            if (layer._id.toString() === layerid) {
+                obj = layer[prop];
+                flag = true;
+            }
+        }
+    }
+    else {
+        // 找到前一帧，并复制前一帧的属性值
+        for (let i = index - 1; i >= 0; i--) {
+            const item = keyframes[i];
+            if (item) {
+                for (let layer of item.layers) {
+                    if (layer.layerid === layerid && layer[prop]) {
+                        obj = layer[prop];
+                        flag = true;
+                    }
                 }
             }
         }
@@ -53,7 +66,7 @@ router.post('/add', function* () {
             obj = {
                 value: 100,
                 fx
-            }
+            };
         }
         else {
             obj = {
@@ -65,27 +78,26 @@ router.post('/add', function* () {
                     value: 0,
                     fx
                 }
-            }
+            };
         }
     }
-
-    console.log(obj);
 
     if (value) {
         if (key) {
             obj[key] = {
                 value,
                 fx
-            }
+            };
         }
         else {
             obj = {
                 value,
                 fx
-            }
+            };
         }
     }
 
+    console.log(prop, obj);
     // 先判断是不是在index下含有关键帧, 没有就新增一项
     if (!keyframe) {
         const newKeyframe = yield keyframeModel.add({ index, projectid });
@@ -93,7 +105,7 @@ router.post('/add', function* () {
             kfid: newKeyframe._id,
             layerid,
             [prop]: obj
-        })
+        });
     }
     else {
         // 如果确定有关键帧的话，确定是不是有这个图层的关键帧
@@ -106,10 +118,26 @@ router.post('/add', function* () {
             });
         }
         else {
+
+            // FIX: 如果这个帧存在 应该和之前的属性进行合并 然后将新值赋上去
+            // TODO: 这个 add 方法应该重构一下
+            let tmpProp = Object.assign(obj, keyframeLayer[prop]);
+            if (key) {
+                tmpProp[key] = {
+                    value,
+                    fx
+                };
+            }
+            else {
+                tmpProp[key] = {
+                    value,
+                    fx
+                };
+            }
             yield keyframeLayerModel.update({
                 kfid: keyframe._id,
                 props: {
-                    [prop]: obj
+                    [prop]: tmpProp
                 }
             });
         }
